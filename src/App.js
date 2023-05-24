874674e51cdb40cc6b1e443bf6975d8975682194
Abdoulaye-MAcOS <njigouhrazak@iut-dhaka.edu>
2023-05-24 15:01:14 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="link">Link</Button>
+    </div>
+  );
+}
+
+export default App;
